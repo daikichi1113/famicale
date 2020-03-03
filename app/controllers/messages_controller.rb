d8ab747c43792f20c91668d3eaff3442cc6b4c22
daikichi1113 <happy_daikichi@yahoo.co.jp>
2020-03-03 19:29:38 +0900

    3rd commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,7 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to root_path(@group)
+      redirect_to group_messages_path(@group)
     else
       @messages = @group.messages.includes(:user)
       render :index
